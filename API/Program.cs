ed3a78a806dc70fd41e99d32ad8759c273a56ecf
Juan Daniel Vargas <juandanielvargascordero@gmail.com>
2020-03-21 19:36:13 -0600

    Adding the client application

--- API/Program.cs
+++ API/Program.cs
@@ -17,7 +17,7 @@ namespace API
         public static void Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
-            using(var scope = host.Services.CreateScope())
+            using (var scope = host.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
                 try
@@ -25,7 +25,7 @@ namespace API
                     var context = services.GetRequiredService<DataContext>();
                     context.Database.Migrate();
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
                     var logger = services.GetRequiredService<ILogger<Program>>();
                     logger.LogError(ex, "An error occured during migration");

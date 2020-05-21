aa50c14ba5c07855942286d882df8e1bcca5031a
Juan Daniel Vargas <juandanielvargascordero@gmail.com>
2020-05-20 21:59:32 -0600

    Adding authentication

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -1,14 +1,36 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Domain;
+using Microsoft.AspNetCore.Identity;
 
 namespace Persistence
 {
     public class Seed
     {
-        public static void SeedData(DataContext context)
+        public static async Task SeedData(DataContext context, UserManager<AppUser> userManager)
         {
-            var activities = new List<Activity>
+            if (!userManager.Users.Any())
+            {
+                var users = new List<AppUser>
+                {
+                    new AppUser
+                    {
+                        DisplayName = "Juan",
+                        UserName = "juan",
+                        Email = "juan@test.com"
+                    }
+                };
+                foreach(var user in users)
+                {
+                    await userManager.CreateAsync(user, "Pa$$w0rd");
+                }
+            }
+
+            if (!context.Activities.Any())
+            {
+                var activities = new List<Activity>
             {
                 new Activity
                 {
@@ -101,8 +123,9 @@ namespace Persistence
                     Venue = "Cinema",
                 }
             };
-            context.Activities.AddRange(activities);
-            context.SaveChanges();
+                context.Activities.AddRange(activities);
+                context.SaveChanges();
+            }
         }
     }
 }
